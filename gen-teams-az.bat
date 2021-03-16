ea80bcb66fa561845910f5675ec5dd0bc3cda881
Laitinen Petri <LaitinenP@karelia.fi>
2021-03-16 15:41:22 +0200

    Added some verbose comments

--- gen-teams-az.bat
+++ gen-teams-az.bat
@@ -1,2 +1,17 @@
+echo off
+echo Using the org-conf.json file for generating random teams...
+echo on
+
 py teamify.py org-conf.json > teams.json
 py az-teams.py teams.json > az-create-teams.bat
+
+echo off
+echo ... done. If successfull, the teams.json file contains now teams-structure
+echo and az-create-teams.bat file contains a set of az commands for creating azure
+echo devops teams. Team members has to be added manually from the GUI since Ms has
+echo not (yet) implemented az commands for the job.
+echo .
+echo You should now review the az-create-teams.bat contents and run the script.
+echo .
+echo Please use gen-del-teams-az.bat for generating az commands for removing the Azure
+echo teams and users from the organization.
